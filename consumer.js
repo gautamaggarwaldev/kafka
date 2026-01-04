15bff059f7c96d52bdfb87cac15e62ad535460c7
GautamAggarwalDev <gautam.aggarwal.tech@gmail.com>
2026-01-04 17:29:58 +0530

    create consumer to consume the messages

---
+++ consumer.js
@@ -0,0 +1,24 @@
+const { Kafka } = require("kafkajs");
+
+const kafkaClient = new Kafka({
+  clientId: "my-app",
+  brokers: ["192.168.29.232:9092"],
+});
+
+async function init() {
+    const consumer = kafkaClient.consumer({ groupId: "rider-group" });
+    console.log("Consumer Connecting...");
+    await consumer.connect();
+    console.log("Consumer Connected!");
+    console.log("Subscribing to Topic...");
+    await consumer.subscribe({ topic: "rider-updates", fromBeginning: true});
+    console.log("Subscribed to Topic!");
+    console.log("Running Consumer...");
+    await consumer.run({
+        eachMessage: async ({ topic, partition, message }) => {
+            console.log(`[${topic}, PART: ${partition}, message: ${message.value.toString()}]`);
+        },
+    }); 
+};
+
+init();
